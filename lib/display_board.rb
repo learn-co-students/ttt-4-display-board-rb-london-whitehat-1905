274f2b6b0fabe42072726d7c734ac71ed6bb3a93
bmordan <bernardmordan@gmail.com>
2019-04-16 21:57:32 +0100

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+    to_cell = Proc.new {|row| row.map {|cell| " #{cell} "}.join("|")}
+    
+    board.each_slice(3)
+    .map(&to_cell)
+    .zip(Array.new(2).fill("-" * 11))
+    .each {|row| puts row}
+end
