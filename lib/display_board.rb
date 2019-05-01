3455cd2ff9c72b0b18ce7e2c9bbc359350b4ef1f
NJ-JayJay <metalmedley1@gmail.com>
2019-05-01 14:21:19 +0100

    Literally the same as the previous one but with an argument

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+    for i in (0...9)
+      if (i+1) % 3 == 0 and (i+1) != 9
+        print " " + board[i] + " "
+        print "\n" + "-"*11 + "\n"
+      elsif (i+1) == 9
+        print " " + board[i] + " \n"
+      else
+        print " " + board[i] + " |"
+      end
+    end
+end
